d6828ad1c0852075ae7649637a231314271246b6
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-10 17:00:37 +0800

    add countCart

--- main/main.js
+++ main/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+const inArray = (id, array) => {
+    for (let i = 0; i < array.length; i++) {
+        if (id == array[i].id)
+            return i;
+    }
+    return -1;
+}
+
+const countCart = (tags) => {
+    let countTag = [];
+    tags.forEach(tag => {
+        let token = tag.indexOf('-');
+        if (token < 0) {
+            let index = inArray(tag, countTag);
+            if (index >= 0) {
+                countTag[index].count++;
+            } else {
+                countTag.push({
+                    'id': tag,
+                    'count': 1
+                });
+            }
+        } else {
+            let index = inArray(tag.substring(0, token), countTag);
+            let number = parseFloat(tag.substring(token + 1));
+            if (index > 0) {
+                countTag[index].count += number;
+            } else {
+                countTag.push({
+                    'id': tag.substring(0, token),
+                    'count': number
+                });
+            }
+        }
+    })
+    return countTag;
+}
