e6ca3b0a35bd196c04f2dc96fa286d31042aa66b
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-11 12:41:05 +0800

    rebuild 1

--- main/main.js
+++ main/main.js
@@ -1,57 +1,40 @@
 'use strict';
 
-const inArray = (id, array) => {
-    for (let i = 0; i < array.length; i++) {
-        if (id == array[i].id)
-            return i;
-    }
-    return -1;
-}
 
+const addCountTag = (countTag, tag, count) => {
+    let index = indexOfCountTag(tag, countTag);
+    if (index >= 0) {
+        countTag[index].count += count;
+    } else {
+        countTag.push({ 'id': tag, 'count': count });
+    }
+}
+const indexOfCountTag = (id, countTag) => {
+    return countTag.findIndex((tag, index, arr) => {
+        return tag.id == id;
+    });
+}
 const countCart = (tags) => {
     const countTag = [];
     tags.forEach(tag => {
-        let token = tag.indexOf('-');
-        if (token < 0) {
-            let index = inArray(tag, countTag);
-            if (index >= 0) {
-                countTag[index].count++;
-            } else {
-                countTag.push({
-                    'id': tag,
-                    'count': 1
-                });
-            }
-        } else {
-            let index = inArray(tag.substring(0, token), countTag);
-            let number = parseFloat(tag.substring(token + 1));
-            if (index > 0) {
-                countTag[index].count += number;
-            } else {
-                countTag.push({
-                    'id': tag.substring(0, token),
-                    'count': number
-                });
-            }
-        }
+        let tagArr = tag.split('-');
+        if (tagArr.length == 1)
+            addCountTag(countTag, tag, 1);
+        else
+            addCountTag(countTag, tagArr[0], Number(tagArr[1]));
     })
     return countTag;
 }
 
+
 const getItem = (barcode, items) => {
-    for (let i = 0; i < items.length; i++) {
-        if (barcode == items[i].barcode)
-            return Object.assign({}, items[i]);
-    }
-    return null;
+    return items.find(item => {
+        return barcode == item.barcode;
+    })
 }
 
-const isContain = (barcodes, barcode) => {
-    for (let i = 0; i < barcodes.length; i++) {
-        if (barcode == barcodes[i])
-            return true;
-    }
-    return false;
+const isPromotions = (barcodes, barcode) => {
+    return barcodes.includes(barcode);
 }
 
 const getCart = countTag => {
@@ -60,11 +43,11 @@ const getCart = countTag => {
     const cart = [];
     countTag.forEach(tag => {
         let item = getItem(tag.id, items);
-        if (item != null) {
+        if (item != undefined) {
             item.count = tag.count;
             item.total = item.price * item.count;
             // 有优惠
-            if (isContain(promotions[0].barcodes, tag.id)) {
+            if (isPromotions(promotions[0].barcodes, tag.id)) {
                 item.total = item.total - Math.floor(item.count / 3) * item.price;
             }
             cart.push(item);
@@ -73,16 +56,26 @@ const getCart = countTag => {
     return cart;
 }
 
+const getOriginalTotalMoney = (cart) => {
+    return cart.reduce((acc, item) => {
+        return acc + item.price * item.count;
+    }, 0);
+}
+
+const getTotalMoney = (cart) => {
+    return cart.reduce((acc, item) => {
+        return acc + item.total;
+    }, 0);
+}
+
 const getReceipt = cart => {
-    let totalMoney = 0;
-    let money = 0;
     let receipt = `***<没钱赚商店>收据***\n`;
     cart.forEach(item => {
         receipt += `名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${item.total.toFixed(2)}(元)\n`;
-        totalMoney += item.total;
-        money += item.price * item.count;
     })
     receipt += "----------------------\n";
+    let totalMoney = getTotalMoney(cart);
+    let money = getOriginalTotalMoney(cart);
     receipt += `总计：${totalMoney.toFixed(2)}(元)\n节省：${(money - totalMoney).toFixed(2)}(元)\n`;
     receipt += "**********************";
     return receipt;
